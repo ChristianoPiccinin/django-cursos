2b1c8c472ee84d396ec38260215b68f11a6e833e
Christiano Piccinin <christiano.piccinin@agasus.com.br>
2023-11-11 19:28:51 -0300

    update templates and ui

--- cursos/urls.py
+++ cursos/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("acessar_curso/<int:id>", views.acessar_curso, name='acessar_curso'),
     path("desativar_curso/<int:id>", views.desativar_curso, name='desativar_curso'),
     path("deletar_curso/<int:id>", views.deletar_curso, name='deletar_curso'),
-    path("criar_curso/", views.criar_curso),
+    path("criar_curso/", views.criar_curso, name='criar_curso'),
     path("listar_cursos/", views.listar_cursos, name='listar_cursos')
 ]
